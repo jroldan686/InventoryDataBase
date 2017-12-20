07602559b712a51f8552c0e3ac38e9531a080f66
jroldan686 <jroldan686@gmail.com>
2017-12-20 14:41:30 +0100

    InventoryFragments

---
+++ app/src/main/java/jrl/deint/inventoryFragments/ui/dependency/DependencyMultiChoiceModeListener.java
@@ -0,0 +1,60 @@
+package jrl.deint.inventoryFragments.ui.dependency;
+
+import android.support.v7.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AbsListView;
+
+import jrl.deint.inventoryFragments.R;
+import jrl.deint.inventoryFragments.ui.dependency.contract.ListDependencyContract;
+import jrl.deint.inventoryFragments.ui.dependency.presenter.ListDependencyPresenter;
+
+/**
+ * Created by usuario on 20/12/17.
+ */
+
+public class DependencyMultiChoiceModeListener implements AbsListView.MultiChoiceModeListener {
+
+    private ListDependencyPresenter presenter;
+    private int count;
+
+    public DependencyMultiChoiceModeListener(ListDependencyContract.Presenter presenter) {
+
+    }
+
+    @Override
+    public void onItemCheckedStateChanged(android.view.ActionMode actionMode, int position, long id, boolean checked) {
+        if(checked) {
+            presenter.setNewSelection(position);
+        }
+        else {
+            count++;
+            presenter.removeSelection(position);
+        }
+        actionMode.setTitle(count + " seleccionados");
+    }
+
+    @Override
+    public boolean onCreateActionMode(android.view.ActionMode actionMode, Menu menu) {
+        MenuInflater inflater = actionMode.getMenuInflater();
+        inflater.inflate(R.menu.menu_fragment_listdependency, menu);
+        actionMode.setTitle("Iniciando ActionMode");
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(android.view.ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(android.view.ActionMode mode, MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(android.view.ActionMode mode) {
+        count = 0;
+    }
+}
