7331a792a79f8cec5519d3acd4fa594fe84c6922
jroldan686 <jroldan686@gmail.com>
2018-02-05 12:53:11 +0100

    InventoryProviderContact

---
+++ app/src/main/java/jrl/deint/inventoryDataBase/data/provider/InventoryProvider.java
@@ -0,0 +1,54 @@
+package jrl.deint.inventoryDataBase.data.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by usuario on 5/02/18.
+ */
+
+public class InventoryProvider extends ContentProvider {
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        switch (uri) {
+            case DEPENDENCY:
+                break;
+            case DEPENDENCY_ID:
+                break;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
---
+++ app/src/main/java/jrl/deint/inventoryDataBase/data/provider/InventoryProviderContract.java
@@ -0,0 +1,10 @@
+package jrl.deint.inventoryDataBase.data.provider;
+
+/**
+ * Created by usuario on 5/02/18.
+ */
+
+public final class InventoryProviderContract {
+    public static final String AUTHORITY = "";
+    public static final String
+}
